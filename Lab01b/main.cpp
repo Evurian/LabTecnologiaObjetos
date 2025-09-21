806048020fd15a6bc274dd6916c56b8e3496d8ac
Evurian <equispemar@unsa.edu.pe>
2025-09-20 19:41:09 -0500

    Laboratorio 01b

--- Lab01b/main.cpp
+++ Lab01b/main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+void printChange(int money, int coins[], int size, vector<int>& current, int& total) {
+    if (money == 0) {
+        total++;
+        cout << "Forma " << total << ": ";
+        for (int c : current) cout << c << " ";
+        cout << endl;
+        return;
+    }
+    if (money < 0 || size <= 0) {
+        return;
+    }
+
+    current.push_back(coins[size - 1]);
+    printChange(money - coins[size - 1], coins, size, current, total);
+    current.pop_back();
+
+    printChange(money, coins, size - 1, current, total);
+
+
+}
 
 int countChange(int money, int coins[], int size){
 
@@ -9,7 +31,7 @@ int countChange(int money, int coins[], int size){
     if(money < 0 || size <= 0 )  {
         return 0;
     }
-    return countChange(money - coins[size -1],coins ,size) - countChange(money, coins,size - 1) ;
+    return countChange(money - coins[size -1],coins ,size) + countChange(money, coins,size - 1) ;
 
 }
 
@@ -22,5 +44,9 @@ int main(){
 
     int formas = countChange(n, coins, size);
     cout << formas << endl;
+
+    vector<int> current;
+    int total = 0;
+    printChange(n, coins, size, current, total);
     return 0;
 }
